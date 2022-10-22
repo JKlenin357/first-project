4febfc71ada4e75d209f9c9ce9e425679f1b2919
JKlenin357 <jkleynin357@gmail.com>
2022-10-22 10:18:54 +0530

    intent,log,toast done

--- app/src/main/java/com/example/firstproject/MainActivity.java
+++ app/src/main/java/com/example/firstproject/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.firstproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +14,10 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
         int a=10;
+    }
+
+    public void sigupPage(View view) {
+        Intent intent = new Intent(this, SignupPageActivity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/firstproject/SignupPageActivity.java
@@ -0,0 +1,50 @@
+package com.example.firstproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+public class SignupPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup_page);
+        Toast.makeText(this,"onCreate",Toast.LENGTH_SHORT).show();
+        Log.e("TAG","onCreate");
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(this,"onStart",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
